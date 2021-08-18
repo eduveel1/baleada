7c2af54081e3834b4bb79c817d9ddc682aee4d99
Alex <slowdsports@gmail.com>
2021-08-17 18:18:49 -0600

    x

---
+++ ads.js
@@ -0,0 +1,7 @@
+// PROPELLER INTERSTICIAL
+var myScript = document.createElement('script');
+myScript.onload = function() {
+  console.log("Script loaded and ready");
+};
+myScript.src = "//upgulpinon.com/1?z=4455092";
+document.getElementsByTagName('body')[0].appendChild(myScript);
