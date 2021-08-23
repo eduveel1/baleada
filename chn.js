06bba7d3887324883656806c7f78015b988d26e3
Alex <slowdsports@gmail.com>
2021-08-23 08:03:24 -0600

    sdadsa

--- chn.js
+++ chn.js
@@ -2,6 +2,42 @@
 if (location.protocol == 'https:') {
     location.replace(`http:${location.href.substring(location.protocol.length)}`);
 }
+
+window.onload = function() {
+             var player = new Clappr.Player({
+                 source: srcLOAD,
+                 parentId: '#player',
+                 mimeType: 'audio/mpeg',
+                 watermark: "https://i.ibb.co/kqmptsD/iRTVW.png", 
+                 position: 'top-left',
+                 plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl, PlaybackRatePlugin],
+                 events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
+                 height: '100%',
+                 width: '100%',
+                 autoPlay: true,
+                 mute: true,
+                 shakaConfiguration: {
+                 manifest: {retryParameters: {maxAttempts: Infinity}},
+                 streaming: {retryParameters: {maxAttempts: Infinity}},
+                 drm: {retryParameters: {maxAttempts: Infinity}},
+                 },
+                 playbackRateConfig: {
+                 defaultValue: '1.00x',
+                 options: [
+                 {value: '0.10', label: '0.10x'},
+                 {value: '0.25', label: '0.25x'},
+                 {value: '0.50', label: '0.50x'},
+                 {value: '0.75', label: '0.75x'},
+                 {value: '1.00', label: '1.00x'},
+                 {value: '1.25', label: '1.25x'},
+                 {value: '1.50', label: '1.50x'},
+                 {value: '1.75', label: '1.75x'},
+                 {value: '2.00', label: '2.00x'},
+                 ]
+                 },
+             });
+         };
+
 // F O X
 
 let fsoul = "//lnc-fox-soul-scte.tubi.video/index.m3u8";
