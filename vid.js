8100b1ba39534db9b403e9609d414d187eaa3b78
Alex <slowdsports@gmail.com>
2021-08-29 09:29:12 -0600

    Update vid.js

--- vid.js
+++ vid.js
@@ -3,7 +3,7 @@ if (location.protocol == 'https:') {
     location.replace(`http:${location.href.substring(location.protocol.length)}`);
 }
 // E P L
-let p1 = "http://147.182.212.3/live/ovochannel2/playlist.m3u8";
+let p1 = "https://www.livedoomovie.com/02_PremierHD1_720p/chunklist.m3u8";
 let p2 = "https://www.livedoomovie.com/02_PremierHD3_720p/chunklist.m3u8";
 let p3 = "https://www.livedoomovie.com/02_PremierHD1_720p/chunklist.m3u8";
 let p4 = "http://167.99.195.178/live/ovochannel3/playlist.m3u8";
