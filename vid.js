da6ce40ca0844f365a7f567c61c94a4a223e8923
Eduardo Perdomo <33327508+eduveel19@users.noreply.github.com>
2021-09-02 12:40:46 -0600

    Update vid.js

--- vid.js
+++ vid.js
@@ -3,11 +3,11 @@ if (location.protocol == 'https:') {
     location.replace(`http:${location.href.substring(location.protocol.length)}`);
 }
 // E P L
-let p1 = "http://134.122.95.70/live/ovo3/playlist.m3u8";
-let p2 = "http://134.122.95.70/live/ovo1/playlist.m3u8";
-let p3 = "http://134.122.95.70/live/ovo2/playlist.m3u8";
-let p4 = "http://167.99.195.178/live/ovochannel3/playlist.m3u8";
-let p5 = "https://sportsgrid-klowdtv.amagi.tv/playlist.m3u8";
+let p1 = "http://167.99.136.37/live/ovo1/playlist.m3u8";
+let p2 = "http://161.35.26.98/live/ovo2/playlist.m3u8";
+let p3 = "http://134.122.95.6/live/ovo3/playlist.m3u8";
+let p4 = "http://167.99.136.37/live/ovo4/playlist.m3u8";
+let p5 = "http://147.182.215.210/live/ovochannel1/playlist.m3u8";
 let p6 = "https://sportsgrid-klowdtv.amagi.tv/playlist.m3u8";
 let p7 = "https://sportsgrid-klowdtv.amagi.tv/playlist.m3u8";
 let p8 = "https://sportsgrid-klowdtv.amagi.tv/playlist.m3u8";
