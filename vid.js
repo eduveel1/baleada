1ba08d3c1d9f3e0d69a32fbc281f403b61d024ee
eduveel1 <86636030+eduveel1@users.noreply.github.com>
2021-09-08 12:52:40 -0600

    Update vid.js

--- vid.js
+++ vid.js
@@ -5,7 +5,7 @@ if (location.protocol == 'https:') {
 // E P L
 let p1 = "http://134.209.254.135/live/ovo1/playlist.m3u8";
 let p2 = "http://134.209.223.67/live/ovo2/playlist.m3u8";
-let p3 = "http://134.209.254.135/live/ovo3/playlist.m3u8";
+let p3 = "http://134.209.220.115/live/ovo3/playlist.m3u8";
 let p4 = "http://68.183.125.254/live/ovo4/playlist.m3u8";
 let p5 = "http://147.182.215.210/live/ovochannel1/playlist.m3u8";
 let p6 = "https://sportsgrid-klowdtv.amagi.tv/playlist.m3u8";
