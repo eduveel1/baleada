ce3b89e6890e6279a999c5796c3cc161b2cd0ca7
Alex <slowdsports@gmail.com>
2021-09-16 09:26:22 -0600

    Create canales.js

---
+++ salvavida/canales.js
@@ -0,0 +1,7 @@
+let olimpia = `
+<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="hn flag"></i> Local | HD</li>
+</a>
+`;
+for (const ele of document.getElementsByClassName("saltv-olimpia")){ele.innerHTML=(olimpia)}
