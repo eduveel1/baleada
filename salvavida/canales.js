1b6e594440befd441ed1d508a9bf2f47d227550d
Alex <slowdsports@gmail.com>
2021-09-17 19:53:22 -0600

    wedf

--- salvavida/canales.js
+++ salvavida/canales.js
@@ -1,5 +1,30 @@
+// LINK DE LOS CANALES
+
+let telecadena = "telecadena.m3u8";
+let tsi = "tsi.m3u8";
+let canal5 = "canal5.m3u8"
+
+let tigohn = "go:tigohn";
+let tdtvp = "go:tdtvp";
+
+// CANAL DEL EQUIPO
+
+var olimpia = telecadena;
+var marathon = telecadena;
+var motagua = telecadena;
+var vida = telecadena;
+var lobos = telecadena;
+var realespana = telecadena;
+var realespana = minas;
+var platense = telecadena;
+var realsociedad = telecadena;
+var hondurasprogreso = telecadena;
+var victoria = telecadena;
+
+ 
+
 let SALOlimpia = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=olimpia">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -7,7 +32,7 @@ let SALOlimpia = `
 for (const ele of document.getElementsByClassName("saltv-olimpia")){ele.innerHTML=(SALOlimpia)}
 
 let SALMarathon = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=marathon">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -15,7 +40,7 @@ let SALMarathon = `
 for (const ele of document.getElementsByClassName("saltv-marathon")){ele.innerHTML=(SALMarathon)}
 
 let SALMotagua = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=motagua">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -23,7 +48,7 @@ let SALMotagua = `
 for (const ele of document.getElementsByClassName("saltv-motagua")){ele.innerHTML=(SALMotagua)}
 
 let SALVida = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=vida">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -31,7 +56,7 @@ let SALVida = `
 for (const ele of document.getElementsByClassName("saltv-vida")){ele.innerHTML=(SALVida)}
 
 let SALLobos = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=lobos">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -39,7 +64,7 @@ let SALLobos = `
 for (const ele of document.getElementsByClassName("saltv-lobos")){ele.innerHTML=(SALLobos)}
 
 let SALRealEspana = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=realespana">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -47,15 +72,15 @@ let SALRealEspana = `
 for (const ele of document.getElementsByClassName("saltv-realespana")){ele.innerHTML=(SALRealEspana)}
 
 let SALRealdeMinas = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=minas">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
 `;
-for (const ele of document.getElementsByClassName("saltv-realdeminas")){ele.innerHTML=(SALRealdeMinas)}
+for (const ele of document.getElementsByClassName("saltv-minas")){ele.innerHTML=(SALRealdeMinas)}
 
 let SALPlatense = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=platense">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -63,7 +88,7 @@ let SALPlatense = `
 for (const ele of document.getElementsByClassName("saltv-platense")){ele.innerHTML=(SALPlatense)}
 
 let SALRealSociedad = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=realsociedad">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -71,7 +96,7 @@ let SALRealSociedad = `
 for (const ele of document.getElementsByClassName("saltv-realsociedad")){ele.innerHTML=(SALRealSociedad)}
 
 let SALHondurasProgreso = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=hondurasprogreso">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
@@ -79,7 +104,7 @@ let SALHondurasProgreso = `
 for (const ele of document.getElementsByClassName("saltv-hondurasprogreso")){ele.innerHTML=(SALHondurasProgreso)}
 
 let SALVictoria = `
-<a href="//slowdsports.000webhostapp.com/gm/nfl.php?g=steelers">
+<a href="//slowdsports.000webhostapp.com/salvavida.php?g=victoria">
 <li class="canalito list-group-item darkolorbg blancolor">
 <i class="hn flag"></i> Local | HD</li>
 </a>
